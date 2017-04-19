7ea5b3b52cff6da8b7370ee2a4e32515e7e6c08d
janki joshi <janki joshi>
2017-04-18 21:02:02 -0400

    Initial commit

---
+++ JJoshiAssignment3/nPuzzleGameForm.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace JJoshiAssignment3
+{
+    partial class nPuzzleGameForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.txtRow = new System.Windows.Forms.TextBox();
+            this.txtColumn = new System.Windows.Forms.TextBox();
+            this.lblRow = new System.Windows.Forms.Label();
+            this.lblColumn = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuSave = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuLoad = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
+            this.mnuExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog = new System.Windows.Forms.SaveFileDialog();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.btnGenerate.Location = new System.Drawing.Point(336, 58);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(91, 43);
+            this.btnGenerate.TabIndex = 0;
+            this.btnGenerate.Text = "Generate";
+            this.btnGenerate.UseVisualStyleBackColor = false;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // txtRow
+            // 
+            this.txtRow.Location = new System.Drawing.Point(143, 40);
+            this.txtRow.Name = "txtRow";
+            this.txtRow.Size = new System.Drawing.Size(83, 20);
+            this.txtRow.TabIndex = 1;
+            // 
+            // txtColumn
+            // 
+            this.txtColumn.Location = new System.Drawing.Point(143, 81);
+            this.txtColumn.Name = "txtColumn";
+            this.txtColumn.Size = new System.Drawing.Size(83, 20);
+            this.txtColumn.TabIndex = 2;
+            // 
+            // lblRow
+            // 
+            this.lblRow.AutoSize = true;
+            this.lblRow.Location = new System.Drawing.Point(27, 40);
+            this.lblRow.Name = "lblRow";
+            this.lblRow.Size = new System.Drawing.Size(43, 13);
+            this.lblRow.TabIndex = 3;
+            this.lblRow.Text = "Rows : ";
+            // 
+            // lblColumn
+            // 
+            this.lblColumn.AutoSize = true;
+            this.lblColumn.Location = new System.Drawing.Point(27, 81);
+            this.lblColumn.Name = "lblColumn";
+            this.lblColumn.Size = new System.Drawing.Size(47, 13);
+            this.lblColumn.TabIndex = 4;
+            this.lblColumn.Text = "Columns";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(520, 24);
+            this.menuStrip1.TabIndex = 5;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuSave,
+            this.mnuLoad,
+            this.toolStripMenuItem1,
+            this.mnuExit});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // mnuSave
+            // 
+            this.mnuSave.Name = "mnuSave";
+            this.mnuSave.Size = new System.Drawing.Size(152, 22);
+            this.mnuSave.Text = "Save";
+            this.mnuSave.Click += new System.EventHandler(this.mnuSave_Click);
+            // 
+            // mnuLoad
+            // 
+            this.mnuLoad.Name = "mnuLoad";
+            this.mnuLoad.Size = new System.Drawing.Size(152, 22);
+            this.mnuLoad.Text = "Load";
+            this.mnuLoad.Click += new System.EventHandler(this.mnuLoad_Click);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(149, 6);
+            // 
+            // mnuExit
+            // 
+            this.mnuExit.Name = "mnuExit";
+            this.mnuExit.Size = new System.Drawing.Size(152, 22);
+            this.mnuExit.Text = "Exit";
+            this.mnuExit.Click += new System.EventHandler(this.mnuExit_Click);
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "dlgOpen";
+            // 
+            // saveFileDialog
+            // 
+            this.saveFileDialog.FileName = "dlgSave";
+            // 
+            // nPuzzleGameForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(520, 394);
+            this.Controls.Add(this.lblColumn);
+            this.Controls.Add(this.lblRow);
+            this.Controls.Add(this.txtColumn);
+            this.Controls.Add(this.txtRow);
+            this.Controls.Add(this.btnGenerate);
+            this.Controls.Add(this.menuStrip1);
+            this.Name = "nPuzzleGameForm";
+            this.Text = "n - puzzle Game";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.TextBox txtRow;
+        private System.Windows.Forms.TextBox txtColumn;
+        private System.Windows.Forms.Label lblRow;
+        private System.Windows.Forms.Label lblColumn;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem mnuSave;
+        private System.Windows.Forms.ToolStripMenuItem mnuLoad;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem mnuExit;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog;
+    }
+}
+
---
+++ JJoshiAssignment3/nPuzzleGameForm.cs
@@ -0,0 +1,450 @@
+﻿/* 
+ * nPuzzleGameForm.cs
+ * Assignment 3
+ * Revision History,
+ *      Janki Joshi, 2016.10.24: designed
+ *      Janki Joshi, 2016.10.26: created
+ *      Janki Joshi, 2016.10.27: created[continued]
+ *      Janki Joshi, 2016.10.28: created[continued]
+ *      Janki Joshi, 2016.10.29: created[continued]
+ *      Janki Joshi, 2016.10.30: design [modified], added comments, debugged                               
+ *                               
+ */
+
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/// <summary>
+/// the starting of the program
+/// </summary>
+namespace JJoshiAssignment3
+{   
+    /// <summary>
+    /// --> class creates the nPuzzle game with the help of buttons
+    /// and various methods are created to handle the game. 
+    /// --> 2 solvable text files are zipped as not all the random generated games are solvable!
+    /// </summary>
+    public partial class nPuzzleGameForm : Form
+    {
+        // button list stores all the buttons with all its properties
+        List<Button> puzzleButton = new List<Button>();
+
+        int staticRow = 0;
+        int staticColumn = 0;
+
+        Button buttonClicked; 
+                   
+        int left = 70;
+        int top;
+
+        Button free;
+
+        /// <summary>
+        /// default constructor of the class that initializes the form and all components on it.
+        /// </summary>
+        public nPuzzleGameForm()
+        {
+            InitializeComponent();            
+        }        
+
+
+        /// <summary>
+        /// this click event calls the generateButton method to generate the tiles and clear tiles 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {            
+            foreach (Button item in puzzleButton)
+            {
+                item.Click -= pButton_Click;
+                this.Controls.Remove(item);                
+                item.Dispose();               
+            }
+
+            top = 130;
+            puzzleButton.Clear();                     
+
+            int row = int.Parse(txtRow.Text);
+            int column = int.Parse(txtColumn.Text);
+
+            staticRow = row;
+            staticColumn = column;
+
+            generateButtons(row, column);         
+            
+        }
+
+
+        /// <summary>
+        /// generate tiles at a particular loaction on form
+        /// </summary>
+        /// <param name="row">specifies the number of rows</param>
+        /// <param name="column">specifies the number of columns</param>
+        void generateButtons(int row, int column)
+        {
+            int total = row * column;      
+
+            List<int> list = new List<int>();
+            Random randomNumber = new Random();
+            int random = randomNumber.Next(1, total + 1);
+            list.Add(random);
+            int count = 0;
+            int print = 0;
+
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < column; j++)
+                {
+                    // calling the button class
+                    Button t = new Button();
+
+                    // it is tracking the index for the loop
+                    print++;
+
+                    do
+                    {
+                        random = randomNumber.Next(1, total + 1);
+                        if (!list.Contains(random))
+                        {
+                            list.Add(random);
+                        }
+                        count++;
+                    } while (count <= 10 * total);
+
+                    t.Left = left;
+                    t.Top = top;
+                    t.Width = 60;
+                    t.Height = 60;
+                    t.Text = list[print - 1].ToString();
+                    t.BackColor = Color.LightPink;
+                    t.Font = new Font(t.Font, FontStyle.Bold);
+                    t.Name = t.Text;
+                    puzzleButton.Add(t);
+
+                    if (int.Parse(t.Text) == total)
+                    {
+                        t.Visible = false;
+                        free = t;
+                    }
+                    this.Controls.Add(t);
+                    t.Click += pButton_Click;
+                    left += 60;
+                }
+                top += 60;
+                left = 70;
+            }
+        }        
+
+        /// <summary>
+        /// this button click event checks the empty slot of tile and swaps the tiles
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public void pButton_Click(object sender, EventArgs e)
+        {
+            buttonClicked = (Button)sender;
+            const int top = 60;
+            const int left = 60;
+
+            int total = puzzleButton.Count;
+
+            int clickedIndex;
+            int freeIndex = 0;
+            
+            // empty slot at top of the button Clicked
+            if (buttonClicked.Left == free.Left && free.Top + free.Height == buttonClicked.Top)
+            {
+                buttonClicked.Top -= top;
+                free.Top += top;
+                clickedIndex = puzzleButton.IndexOf(buttonClicked);
+                freeIndex = puzzleButton.IndexOf(free);
+                swapButtons(puzzleButton, free, clickedIndex, freeIndex);
+                //freeIndex = int.Parse(free.Text);
+                checkWinner(total, puzzleButton, free);                      
+            }
+
+            // empty slot at right of the button Clicked
+            else if (buttonClicked.Top == free.Top && buttonClicked.Left + buttonClicked.Width == free.Left)
+            {
+                buttonClicked.Left += left;
+                free.Left -= left;
+                clickedIndex = puzzleButton.IndexOf(buttonClicked);
+                freeIndex = puzzleButton.IndexOf(free);
+                swapButtons(puzzleButton, free, clickedIndex, freeIndex);
+                //freeIndex = int.Parse(free.Text);
+                checkWinner(total, puzzleButton, free);
+            }
+
+            // empty slot at left of the button Clicked
+            else if (buttonClicked.Top == free.Top && buttonClicked.Left == free.Left + free.Width)
+            {
+                buttonClicked.Left -= left;
+                free.Left += left;
+                clickedIndex = puzzleButton.IndexOf(buttonClicked);
+                freeIndex = puzzleButton.IndexOf(free);
+                swapButtons(puzzleButton, free, clickedIndex, freeIndex);                
+                checkWinner(total, puzzleButton, free);
+            }
+
+            // empty slot at bottm of the button Clicked
+            else
+            {
+                if (buttonClicked.Left == free.Left && buttonClicked.Top + buttonClicked.Height == free.Top)
+                {
+                    buttonClicked.Top += top;
+                    free.Top -= top;
+                    clickedIndex = puzzleButton.IndexOf(buttonClicked);
+                    freeIndex = puzzleButton.IndexOf(free);
+                    swapButtons(puzzleButton, free, clickedIndex, freeIndex);                    
+                    checkWinner(total, puzzleButton, free);
+                }
+            }                
+        }        
+
+        /// <summary>
+        /// this method swaps the tiles and arranges the list
+        /// </summary>
+        /// <param name="puzzleButton"> button list </param>
+        /// <param name="free">empty slot button</param>
+        /// <param name="clickedIndex">index of the button that is clicked</param>
+        /// <param name="freeIndex">index of the current empty slot</param>
+        void swapButtons(List<Button> puzzleButton, Button free, int clickedIndex, int freeIndex)
+        {
+            Button swapHelp;
+            swapHelp = puzzleButton[clickedIndex];
+            puzzleButton[clickedIndex] = puzzleButton[freeIndex];            
+            puzzleButton[freeIndex] = swapHelp;            
+        }
+
+
+        /// <summary>
+        /// this method checks the winner
+        /// </summary>
+        /// <param name="total">total buttons on the form</param>
+        /// <param name="puzzleButton">button list</param>
+        /// <param name="free">empty slot button</param>
+        void checkWinner(int total, List<Button> puzzleButton, Button free)
+        {
+            if (puzzleButton[0].Text == "1")
+            {                
+                for (int i = 1; i < total; i++)
+                {                                        
+                    if (int.Parse(puzzleButton[i - 1].Text) > int.Parse(puzzleButton[i].Text))
+                    {
+                        return;
+                    }
+                }
+                MessageBox.Show("Great Job. You Win");              
+            }
+        }
+
+        /// <summary>
+        /// this click event calls the doload method to load the current game
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void mnuLoad_Click(object sender, EventArgs e)
+        {
+            DialogResult result = openFileDialog.ShowDialog();
+            switch (result)
+            {
+                case DialogResult.Abort:
+                    break;
+                case DialogResult.Cancel:
+                    break;
+                case DialogResult.Ignore:
+                    break;
+                case DialogResult.No:
+                    break;
+                case DialogResult.None:
+                    break;
+                case DialogResult.OK:
+                    string filename = openFileDialog.FileName;
+                    doLoad(filename);
+                    break;
+                case DialogResult.Retry:
+                    break;
+                case DialogResult.Yes:
+                    break;
+                default:
+                    break;
+            }
+        }
+
+
+        /// <summary>
+        /// saves the current game
+        /// </summary>
+        /// <param name="filename"></param>
+        private void doSave(string filename)
+        {
+            StreamWriter writer = new StreamWriter(filename);
+
+            writer.WriteLine(txtRow.Text);
+            writer.WriteLine(txtColumn.Text);            
+
+            foreach (Button item in puzzleButton)
+            {               
+                writer.WriteLine(item.Text);
+            }
+
+            writer.Close();
+                        
+        }
+
+
+        /// <summary>
+        /// loads the current game
+        /// </summary>
+        /// <param name="filename"></param>
+        private void doLoad(string filename)
+        {
+            StreamReader reader = new StreamReader(filename);
+
+            int min = int.Parse(reader.ReadLine());
+            int max = int.Parse(reader.ReadLine());
+
+            List<int> listCheck = new List<int>();
+
+            int n = min * max;
+
+            for (int i = 0; i < n; i++)
+            {
+                int item = int.Parse(reader.ReadLine());
+                listCheck.Add(item);
+            }
+
+            loadSavedGame(min, max, listCheck);          
+            reader.Close();
+        }
+
+
+        /// <summary>
+        /// this click event calls the doSave method to save the current game
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void mnuSave_Click(object sender, EventArgs e)
+        {
+            DialogResult result = saveFileDialog.ShowDialog();
+            switch (result)
+            {
+                case DialogResult.Abort:
+                    break;
+                case DialogResult.Cancel:
+                    break;
+                case DialogResult.Ignore:
+                    break;
+                case DialogResult.No:
+                    break;
+                case DialogResult.None:
+                    break;
+                case DialogResult.OK:
+                    try
+                    {
+                        string filename = saveFileDialog.FileName;
+                        doSave(filename);
+                    }
+                    catch (Exception)
+                    {
+                        MessageBox.Show("Error in file save");
+                    }
+                    break;
+                case DialogResult.Retry:
+                    break;
+                case DialogResult.Yes:
+                    break;
+                default:
+                    break;
+            }
+        }
+
+
+        /// <summary>
+        /// this method iniializes all the components based on the saved game.
+        /// </summary>
+        /// <param name="min">it is number of rows saved</param>
+        /// <param name="max">it is the number of columns saved</param>
+        /// <param name="listCheck">the list<int> having all the nmbers saved in the current format</int></param>
+        void loadSavedGame(int min, int max, List<int> listCheck)
+        {
+            foreach (Button item in puzzleButton)
+            {
+                item.Click -= pButton_Click;
+                this.Controls.Remove(item);
+                item.Dispose();
+            }
+
+            txtRow.Text = min.ToString();
+            txtColumn.Text = max.ToString();
+
+            top = 130;
+            puzzleButton.Clear();
+
+            int total = min * max;
+
+            int row = min;
+            int column = max;
+            
+            int print = 0;
+
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < column; j++)
+                {
+                    // calling the button class
+                    Button t = new Button();
+
+                    // it is tracking the index for the loop
+                    print++;                    
+
+                    t.Left = left;
+                    t.Top = top;
+                    t.Width = 60;
+                    t.Height = 60;
+                    t.Text = listCheck[print-1].ToString();
+                    t.BackColor = Color.LightPink;
+                    t.Name = t.Text;
+                    puzzleButton.Add(t);
+
+                    if (int.Parse(t.Text) == total)
+                    {
+                        t.Visible = false;
+                        free = t;
+                    }
+                    this.Controls.Add(t);
+                    t.Click += pButton_Click;
+                    left += 60;
+                }
+                top += 60;
+                left = 70;
+            }
+
+        }
+
+        /// <summary>
+        /// exits the application when pressed OK!
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void mnuExit_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Exit application?", "", MessageBoxButtons.OKCancel) == DialogResult.OK)
+            {
+                Application.Exit();
+            }
+        }
+    }
+
+}
+
